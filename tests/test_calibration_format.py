20993f813777ea755d531bf684996a908ed5ae52
mhairifin <m.finlayson@imperial.ac.uk>
2024-07-24 16:41:52 +0100

    rename tests using conventions specified in dev standards document

--- tests/test_calibration_format.py
+++ tests/test_calibration_format.py
@@ -3,7 +3,7 @@ from src.calibrationFormatProcessor import CalibrationFormatProcessor
 from src.calibrationFormat import Unit, CalibrationFormat
 from pydantic import ValidationError
 
-def test_calibration_read():
+def test_calibration_processor_reads_calibration_file_from_path_when_given_good_input():
     result = CalibrationFormatProcessor.loadFromPath("tests/example/calibrations.json")
 
     assert result != None
@@ -11,12 +11,12 @@ def test_calibration_read():
     assert result.calibrations[0].offsets.X == [2.2345, 2.3768]
     assert isinstance(result.valid_start,datetime)
 
-def test_calibration_invalid_file():
+def test_calibration_processor_returns_empty_calibration_when_given_invalid_filepath():
     result = CalibrationFormatProcessor.loadFromPath("nonsense/path")
 
     assert result == None
     
-def test_calibration_dict_loading():
+def test_calibration_processor_loads_valid_input_from_correctly_formatted_dictionary():
     calib_dict = {
         "valid_start":datetime.now(),
         "valid_end": datetime.now(),
@@ -43,7 +43,7 @@ def test_calibration_dict_loading():
     assert calibration.calibrations[0].offsets.X == [0]
     assert isinstance(calibration.valid_start,datetime)
 
-def test_failed_dict_length_loading():
+def test_calibration_processor_errors_when_given_offset_lists_of_differing_lengths():
     calib_dict = {
         "valid_start":datetime.now(),
         "valid_end": datetime.now(),
